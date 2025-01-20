b485e43b19f31243c9b2cbc3f07d15260a6c92fb
Dome C <dome@tel.co.th>
2025-01-20 12:52:57 +0700

    add 1D , 1Hr example

--- src/EPOINT1D.sol
+++ src/EPOINT1D.sol
@@ -6,9 +6,10 @@ import "@kiwarilabs/tokens/ERC20/LightWeightERC20EXPBase.sol";
 // blocktime 2 sec
 // 4382901 1 hr
 // 182620 1 day
-contract EPOINT1HR is ERC20EXPBase {
-    uint256 public exp =  3600 ; // secound in 1 day
-    constructor() ERC20EXPBase("Earth Point DEV", "EPOINT1HR", 3081088, 4382901, 1, true) {}
+
+contract EPOINT1D is ERC20EXPBase {
+    uint256 public exp =  86400 ; // secound in 1 day
+    constructor() ERC20EXPBase("Earth Point DEV", "EPOINT1D", 3621337, 182620, 1, true) {}
 
     function mint(address to, uint256 value) public {
         _mint(to, value);
